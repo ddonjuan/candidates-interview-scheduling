a29e38e073cdf111dfc0dd05e5f475ac4fc31337
ddonjuan <daniel.donjuan25@gmail.com>
2018-10-23 15:29:18 -0700

    finished the confirmation page

--- client/src/components/candidate-confirmation-page.js
+++ client/src/components/candidate-confirmation-page.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import dummyData from './dummy-data'
 
-export default (props)=>{
-    return(
-        <h1>This is where the candidate will see the confirmation message</h1>
+export default (props) => {
+    return (
+        <div className="container">
+            <div className="email-confirm">
+                <h3 className="center">Your application has been submitted</h3>
+                <h4 className="center">Reference number to your application has been emailed to: </h4>
+                <h4 className="center">{dummyData[0].email}</h4>
+            </div>
+
+        </div>
     )
 }
