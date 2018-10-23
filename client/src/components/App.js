b500cd9ff988d61117ac22e26b83836a31a8c647
ddonjuan <daniel.donjuan25@gmail.com>
2018-10-23 14:50:24 -0700

    saving essay branch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,33 +9,64 @@ import CandidateConfirmationPage from './candidate-confirmation-page';
 import '../stylesheets/App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state={
+    this.state = {
       essay1: '',
-      essay2: ''
+      essay2: '',
+      textAreaChar: null
     }
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange(event){
-    const {value, name} = event.target;
+  handleInputChange(event) {
+    const { value, name } = event.target;
     this.setState({
-        [name]: value
+      [name]: value
     });
-}
+    this.charCount(name, value);
+  }
+  charCount(name, value) {
+    let maxChar = 2000;
+    let currentValue = value.length;
+    let remainingChar = null;
+    var essay1Page = document.getElementsByClassName('remaining-input')[0];
+    switch (name) {
+      case 'essay1':
+        if (currentValue < maxChar) {
+          remainingChar = maxChar - currentValue
+          essay1Page.innerHTML = remainingChar;
+        }
+
+        break;
+      case 'essay2':
+        if (value.length < maxChar) {
+          essay1Page.innerHTML = value.length;
+        }
+        break;
+      default:
+        break;
+    }
+
+    return;
+
+  }
+  characterTrim(essay) {
+
+  }
   render() {
-    const {essay1, essay2} = this.state;
+    console.log("this is the state: ", this.state.textAreaChar);
+    const { essay1, essay2 } = this.state;
     console.log("this is the state in app: ", this.state);
     return (
       <div className="app">
-        <Header/>
-        <Route exact path="/" component={CandidateLandingPage}/>
-        <Route path="/candidate-step1-info" render={(props)=><CandidateStep1Info {...props} inputChange={this.handleInputChange}/>}/>
-        <Route path="/candidate-step2-essays" render={(props)=> <CandidateStep2Essays {...props} essay1={essay1} essay2={essay2} inputChange={this.handleInputChange}/>}/>
-        <Route path="/candidate-step3-review" render={(props)=><CandidateStep3Review {...props}/>}/>
-        <Route path="/candidate-confirmation-page" render={(props)=><CandidateConfirmationPage {...props}/>}/>
+        <Header />
+        <Route exact path="/" component={CandidateLandingPage} />
+        <Route path="/candidate-step1-info" render={(props) => <CandidateStep1Info {...props} inputChange={this.handleInputChange} />} />
+        <Route path="/candidate-step2-essays" render={(props) => <CandidateStep2Essays {...props} essay1={essay1} essay2={essay2} inputChange={this.handleInputChange} />} />
+        <Route path="/candidate-step3-review" render={(props) => <CandidateStep3Review {...props} />} />
+        <Route path="/candidate-confirmation-page" render={(props) => <CandidateConfirmationPage {...props} />} />
       </div>
     );
   }
