352e4359b6b033ca581c5469c7c312bc26b29a6f
harrison8024 <harrison8024@gmail.com>
2018-10-24 16:32:07 -0700

    submit still have some issue

--- client/src/components/candidate-confirmation-page.js
+++ client/src/components/candidate-confirmation-page.js
@@ -1,36 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import dummyData from './dummy-data';
-import firebase from 'firebase';
+// import firebase from 'firebase';
+// import axios from 'axios';
 
-class CandidateConfirmation extends Component{
+class CandidateConfirmation extends Component {
     constructor(props) {
         super(props);
-      }
-    componentWillMount(){
-        var firstName =this.props.state.firstName;
-        var LastName = this.props.state.lastName;
-        var config = {
-            apiKey: 'AIzaSyAiaonRqttDyUYuezZshYwftS_nG6YFjPs',
-            authDomain: 'interview-app-5def8.firebaseapp.com',
-            databaseURL: 'https://interview-app-5def8.firebaseio.com/',
-            storageBucket: 'gs://interview-app-5def8.appspot.com'
-        };
-        firebase.initializeApp(config);
-        var storage = firebase.storage();
-        var ref = storage.ref();
-        var file = this.props.state.cv;
-        var refName = firstName+"."+LastName+"."+file.name
-        var uploadTask = ref.child(refName).put(file).then(function(snapshot) {
-            console.log('Uploaded a blob or file!', snapshot);
-            ref.child(refName).getDownloadURL().then(function(url) {
-                var address = url
-                console.log(address);
-              }).catch(function(error) {
-                console.log(error);
-              });
-          });
     }
-    render(){
+
+    // async insertStudent(url) {
+    //     var userInfo = {
+    //         firstName: this.props.state.firstName,
+    //         lastName: this.props.state.lastName,
+    //         phone: this.props.state.phone,
+    //         email: this.props.state.email,
+    //         school: this.props.state.school,
+    //         year: this.props.state.yearOfGraduation,
+    //         cv: url,
+    //         essay1: this.props.state.essay1,
+    //         essay2: this.props.state.essay2
+    //     }
+    //     await axios.post('http://localhost:8888/submit-information.php', { userInfo }).then(response => {
+    //         console.log(response);
+    //     });
+    // }
+
+    // componentWillMount() {
+    //     var firstName = this.props.state.firstName;
+    //     var LastName = this.props.state.lastName;
+    //     var config = {
+    //         apiKey: 'AIzaSyAiaonRqttDyUYuezZshYwftS_nG6YFjPs',
+    //         authDomain: 'interview-app-5def8.firebaseapp.com',
+    //         databaseURL: 'https://interview-app-5def8.firebaseio.com/',
+    //         storageBucket: 'gs://interview-app-5def8.appspot.com'
+    //     };
+    //     firebase.initializeApp(config);
+    //     var storage = firebase.storage();
+    //     var ref = storage.ref();
+    //     var file = this.props.state.cv;
+    //     var refName = firstName + "." + LastName + "." + file.name
+    //     var uploadTask = ref.child(refName).put(file).then(function (snapshot) {
+    //         console.log('Uploaded a blob or file!', snapshot);
+    //         ref.child(refName).getDownloadURL().then(function (url) {
+    //             var address = url
+    //             console.log(this);
+    //             this.insertStudent(address);
+    //             console.log(address);
+    //         }.bind(this)).catch(function (error) {
+    //             console.log(error);
+    //         });
+    //     }.bind(this));
+    // }
+    render() {
         return (
             <div className="container">
                 <div className="email-confirm">
@@ -38,7 +59,7 @@ class CandidateConfirmation extends Component{
                     <h4 className="center">Reference number to your application has been emailed to: </h4>
                     <h4 className="center">{dummyData[0].email}</h4>
                 </div>
-    
+
             </div>
         )
     }
--- php/submit-information.php
+++ php/submit-information.php
@@ -1,28 +1,43 @@
 <?php
+print_r("here");
 require('./header.php');
+require('./cross-origin.php');
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
 
 $stmt = $conn->prepare("INSERT INTO `candidate` (`id`, `firstname`, `lastname`, `email`, `phone`, `school`, `year`, `cv`, `essay1`, `essay2`, `interest`, `time`, `status`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?)");
 $stmt->bind_param("sssssisssss", $firstname, $lastname, $email, $phone, $school, $year, $cv, $essay1, $essay2, $interest, $status);
 
-$firstname = "test";
-$lastname = "test";
-$email = "test";
-$phone = "test";
-$school = "test";
-$year = 2019;
-$cv = "test";
-$essay1 = "test";
-$essay2 = "test";
+$firstname = $request['userInfo']['firstName'];
+$lastname = $request['userInfo']['lastName'];
+$email = $request['userInfo']['email'];
+$phone = $request['userInfo']['phone'];
+$school = $request['userInfo']['school'];
+$year = $request['userInfo']['year'];
+$cv = $request['userInfo']['cv'];
+$essay1 = $request['userInfo']['essay1'];
+$essay2 = $request['userInfo']['essay2'];
 $interest = "test";
 $status = "pending";
-$stmt->execute();
 
-print_r($stmt);
+$output['success'] = $request['userInfo'];
+
+
+if($stmt->execute()){
+    $output['success'] = true;
+    $output['request'] = $request;
+}else{
+    $output['error'] = $stmt->error;
+    $output['request'] = $request;
+}
 
 $stmt->close();
 $conn->close();
+
+print(json_encode($output))
 ?>
