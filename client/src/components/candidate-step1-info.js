36f451139ff03f08af7b1349b69a32e2f758f32a
Legacy92 <colemancorey22@gmail.com>
2018-10-25 22:47:23 -0700

    added labels for inputs

--- client/src/components/candidate-step1-info.js
+++ client/src/components/candidate-step1-info.js
@@ -1,97 +1,80 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Nav from './candidate-navigation';
 
 
 class CandidateStep1Info extends Component {
     constructor(props) {
         super(props)
     }
-
     render() {
-        const {enableSubmit} = this.props;
-        const submitButton = enableSubmit ? <Link to="/candidate-step2-essays" className="waves-effect waves-light btn-large">Next -></Link> : <button className="waves-effect waves-light btn-large">Next -></button>
+        console.log("These are the props: ", this.props);
+
         const { inputChange, uploadChange } = this.props;
-        const { firstName, lastName, phone, email, c_email, school, interestedFunctions, yearOfGraduation, cv } = this.props;
+        const { firstName, lastName, phone, email, confirmEmail, school, interestedFunctions, yearOfGraduation, cv } = this.props;
         return (
             <div className="container step-1-page">
-                <Nav location={this.props.location.pathname}/>
-                <h3 className="center">Please fill out form.</h3>
-                <div className="divider"></div>
+                <h1 className="center">Please fill out form carefully.</h1>
                 <form className="col s12" action="">
                     <div className="row">
                         <div className="input-field col s6">
-                            <input placeholder="First Name" onChange={inputChange} name="firstName" value={firstName} id="firstName" type="text" className=""/>
-                            <div className="hidDiv firstName">Field must contain at least one character</div>
+                            <input placeholder="First Name" onChange={inputChange} name="firstName" value={firstName} id="firstName" type="text" className="validate" />
+                            <label for="firstName" className="active">First Name</label>
                         </div>
                         <div className="input-field col s6">
-                            <input placeholder="Last Name" onChange={inputChange} name="lastName" value={lastName} id="lastName" type="text" className=""/>
-                            <div className="hidDiv lastName">Invalid Input. Field must contain at least one character</div>
+                            <input placeholder="Last Name" onChange={inputChange} name="lastName" value={lastName} id="lastName" type="text" className="validate" />
+                            <label for="lastName" className="active">Last Name</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <input placeholder="Phone Number" onChange={inputChange} name="phone" value={phone} id="phone" type="text" className=""/>
-                            <div className="hidDiv phone">Invalid Input. Field must contain a total of 10 digits including area code with the exception of "()" and "-"</div>
+                            <input placeholder="Phone Number" onChange={inputChange} name="phone" value={phone} id="phone" type="text" className="validate" />
+                            <label for="phone" className="active">Phone Number</label>
                         </div>
                         <div className="input-field col s12">
-                            <input placeholder="Email" onChange={inputChange} name="email" value={email} id="email" type="email" className=""/>
-                            <div className="hidDiv email">Invalid Input. Field must contain '@'</div>
+                            <input placeholder="Email" onChange={inputChange} name="email" value={email} id="email" type="email" className="validate" />
+                            <label for="email" className="active">Email</label>
                         </div>
                         <div className="input-field col s12">
-                            <input placeholder="Confirm Email" onChange={inputChange } name="c_email" id="c_email" type="email" className=""/>
-                            <div className="hidDiv c_email">Email address does not match</div>
-                            <div className="hidDiv c_emailRight">Email Matches</div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="input-field col s12 school-input">
-                            <input placeholder="School of Pharmacy" onChange={inputChange} name="school" value={school} id="school" type="text" className=""/>
-                            <div className="hidDiv school">Invalid Input. Field must contain at least one character</div>
-                        </div>
-                        <div className="input-field col s12">
-                            <input placeholder="Expected year of graduation" onChange={inputChange} name="yearOfGraduation" value={yearOfGraduation} id="yearOfGraduation" type="month" className=""/>
-                            <label for="yearOfGradutation" className="active year-label">Expected year of graduation</label>
+                            <input placeholder="Confirm Email" onChange={inputChange} value={confirmEmail} id="confirmEmail" type="email" className="validate" />
+                            <label for="confirmEmail" className="active">Confirm Email</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field col s12">
-                            <select id="interestedFunction" onChange={inputChange} name="interestedFunction" value={interestedFunctions} className="browser-default valid">
+                            <input placeholder="School of Pharmacy" onChange={inputChange} name="school" value={school} id="school" type="text" className="validate" />
+                            <label for="School of Pharmacy" className="active">School of Pharmacy</label>
+                        </div>
+                        <div className="input-field col s12">
+                            <input placeholder="Expected year of graduation" onChange={inputChange} name="yearOfGraduation" value={yearOfGraduation} id="yearOfGraduation" type="month" className="validate" />
+                            <label for="yearOfGradutation" className="active">Expected year of graduation</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <select onChange={inputChange} value={interestedFunctions} className="browser-default">
                                 <option value="" disabled selected>Interested Functions</option>
-                                <option value="Clinical Development">Clinical Development</option>
-                                <option value="Medical Affairs">Medical Affairs (External Scientific Comm., Global Phase IV, Global Medical Comm.)</option>
-                                <option value="Global Regulatory Affairs">Global Regulatory Affairs – CMC</option>
-                                <option value="Clinical Pharmacology"> Clinical Pharmacology</option>
+                                <option value="clinicalDevelopment" name="Clinical Development">Clinical Development</option>
+                                <option value="2">Medical Affairs</option>
+                                <option value="3">Global Regulatory Affairs – CMC</option>
+                                <option value="4"> Clinical Pharmacology</option>
                             </select>
+                            <label for="interestedFunctions" className="active">Interested Functions</label>
                         </div>
                     </div>
-                    <blockquote>
-                            *Notes: Medical Affairs Candidates will select individual function upon
-completion of the 1st round of interview.
-                        </blockquote>
-                    <br/>
                     <div className="row">
                         <div className="file-field input-field">
                             <label for="btn" className="active">Upload CV/Resume</label>
-                            <br/>
                             <div className="btn" name="btn">
                                 <span>File</span>
-                                <input type="file" onChange={uploadChange}/>
+                                <input type="file" onChange={uploadChange} />
                             </div>
                             <div className="file-path-wrapper">
-                                <input type="text" id="file-path" className="file-path" value={cv ? cv.name : ''} onChange={this.handleChange} placeholder="Select a file from your computer"/>
+                                <input type="text" className="file-path validate" value={cv ? cv.name : ''} onChange={this.handleChange} />
                             </div>
-                            <div className="hidDivBlock file">File Successfully Uploaded</div>
-
                         </div>
                     </div>
                 </form>
-                {submitButton}
-                <div className="row">
-                    <div className="col s4 push-s5">
-                        <Link to="/candidate-step2-essays" className="waves-effect waves-light btn-large">Next</Link>
-                    </div>
-                </div>
+                <Link to="/candidate-step2-essays" className="waves-effect waves-light btn-large">Next -></Link>
             </div>
         )
     }
